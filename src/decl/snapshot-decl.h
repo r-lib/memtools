d3e7f9bf53f8117ad21b9e8789950d7c658f83f3
Lionel Henry <lionel.hry@gmail.com>
2021-01-15 11:48:20 +0100

    Add size to stack data

--- src/decl/snapshot-decl.h
+++ src/decl/snapshot-decl.h
@@ -1,3 +1,5 @@
 
+static struct snapshot_data new_snapshot_data();
+static void check_stack(struct snapshot_data* data, int depth);
 static sexp* new_snapshot_node(sexp* id, enum r_type type);
 static r_ssize obj_size(sexp* x);
--- src/snapshot.c
+++ src/snapshot.c
@@ -4,18 +4,22 @@
 
 static sexp* snapshot_node_names = NULL;
 
+#define DICT_SIZE_INIT 1024
+#define STACK_SIZE_INIT 1024
+#define STACK_GROWTH_FACTOR 1.5
+
 struct snapshot_data {
+  sexp* shelter;
   struct r_dict dict;
+  int stack_size;
+  int* p_sizes;
 };
 
 
+// [[ register() ]]
 sexp* snapshot(sexp* x) {
-  struct r_dict dict = r_new_dict(1024);
-  KEEP(dict.shelter);
-
-  struct snapshot_data data = (struct snapshot_data) {
-    .dict = dict
-  };
+  struct snapshot_data data = new_snapshot_data();
+  KEEP(data.shelter);
 
   FREE(1);
   return r_null;
@@ -30,6 +34,8 @@ bool snapshot_iterator(void* data,
                        enum r_node_relation rel,
                        r_ssize i,
                        bool on_return) {
+  check_stack(data, depth);
+
   sexp* id = KEEP(r_sexp_address(x));
   sexp* node = KEEP(new_snapshot_node(id, type));
 
@@ -63,6 +69,57 @@ r_ssize obj_size(sexp* x) {
   return 10;
 }
 
+
+// Snapshot data ----------------------------------------------------------
+
+enum snapshot_shelter {
+  SNAPSHOT_SHELTER_dict = 0,
+  SNAPSHOT_SHELTER_sizes = 1
+};
+
+static
+struct snapshot_data new_snapshot_data() {
+  sexp* shelter = r_new_vector(r_type_list, 2);
+  KEEP(shelter);
+
+  struct r_dict dict = r_new_dict(DICT_SIZE_INIT);
+  r_list_poke(shelter, SNAPSHOT_SHELTER_dict, dict.shelter);
+
+  sexp* sizes = r_new_vector(r_type_integer, STACK_SIZE_INIT);
+  r_list_poke(shelter, SNAPSHOT_SHELTER_sizes, sizes);
+
+  struct snapshot_data data = (struct snapshot_data) {
+    .shelter = shelter,
+    .dict = dict,
+    .stack_size = STACK_SIZE_INIT,
+    .p_sizes = r_int_deref(sizes)
+  };
+
+  FREE(1);
+  return data;
+};
+
+static
+void check_stack(struct snapshot_data* data, int depth) {
+  r_ssize size = data->stack_size;
+  if (depth <= size) {
+    return;
+  }
+
+  size = r_ssize_mult(size, STACK_GROWTH_FACTOR);
+
+  sexp* sizes = r_list_get(data->shelter, SNAPSHOT_SHELTER_sizes);
+
+  sizes = r_int_resize(sizes, size);
+  r_list_poke(data->shelter, SNAPSHOT_SHELTER_sizes, sizes);
+  data->p_sizes = r_int_deref(sizes);
+
+  data->stack_size = size;
+  return;
+}
+
+
+// Initialisation ---------------------------------------------------------
 
 void init_snapshot() {
   const char* snapshot_node_names_code = "c('id', 'type', 'retained_count')";
