81003df20e4b67366630a6fffaf920a3889fad4a
Lionel Henry <lionel.hry@gmail.com>
2021-01-27 14:18:08 +0100

    Expose `sexp_self_size()`

--- R/memtools.R
+++ R/memtools.R
@@ -10,6 +10,10 @@ addr_deref <- function(x) {
   .Call(c_ptr_addr_deref, x)
 }
 
+sexp_self_size <- function(x) {
+  .Call(c_ptr_sexp_self_size, x)
+}
+
 size_node <- function(x) {
   as.integer(utils::object.size(new_node(NULL)))
 }
