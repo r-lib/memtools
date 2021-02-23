f6b8ff7a1ab9ae618a550b2125ff727ea55eda6d
Lionel Henry <lionel.hry@gmail.com>
2021-02-23 11:45:21 +0100

    Add basic tests for `mem_node_dominated_ids()`

--- tests/testthat/test-node.R
+++ tests/testthat/test-node.R
@@ -6,3 +6,33 @@ test_that("must subset with a known field", {
     (expect_error(root$foobar))
   })
 })
+
+test_that("mem_node_dominated_ids() works", {
+  e1 <- env(empty_env(), a = 1)
+  e2 <- env(empty_env())
+  e1$b <- e2
+
+  s <- mem_snapshot(e1)
+  root <- s$node[[1]]
+
+  expect_null(root$dominator)
+
+  root_dominated <- mem_node_dominated_ids(root)
+  expect_equal(
+    sort(root_dominated),
+    sort(s$id[-1])
+  )
+
+  leaf <- s$node[[which(s$type == "double")]]
+  expect_equal(
+    leaf$dominated,
+    list()
+  )
+
+  e2_node <- s$node[[which(s$type == "environment")[[2]]]]
+  e2_dominated <- mem_node_dominated_ids(e2_node)
+  expect_equal(
+    e2_dominated,
+    e2_node$children[[1]]$to$id
+  )
+})
