1eddfab61b2618fcd7bc7d6997124151fdd9ee2d
Lionel Henry <lionel.hry@gmail.com>
2021-03-02 18:11:18 +0100

    Draft documentation of `mem_snapshot()`

--- R/snapshot.R
+++ R/snapshot.R
@@ -1,3 +1,8 @@
+#' Create a memory snapshot
+#' @param x An R object. This becomes the root of the graph of R
+#'   objects captured in the snapshot.
+#' @return A data frame containing nodes, arrows between these nodes,
+#'   and metadata.
 #' @export
 mem_snapshot <- function(x) {
   .Call(c_ptr_snapshot, x)
