6af0612f0a13819ad840eb7edcf6ef7341882e13
Lionel Henry <lionel.hry@gmail.com>
2021-03-02 18:14:43 +0100

    Document `mem_stash()`

--- R/snapshot.R
+++ R/snapshot.R
@@ -3,11 +3,21 @@
 #'   objects captured in the snapshot.
 #' @return A data frame containing nodes, arrows between these nodes,
 #'   and metadata.
+#' @seealso [mem_stash()] to prevent objects from being recorded in a
+#'   snapshot.
 #' @export
 mem_snapshot <- function(x) {
   .Call(c_ptr_snapshot, x)
 }
 
+#' Stash objects
+#'
+#' Objects contained in a stash are never recorded by
+#' [mem_snapshot()].
+#'
+#' @param ... Objects to stash.
+#' @return An environment containing the stashed objects. You can add
+#'   objects to this environment in the usual fashion.
 #' @export
 mem_stash <- function(...) {
   .Call(ffi_new_stash, env(empty_env(), ...))
